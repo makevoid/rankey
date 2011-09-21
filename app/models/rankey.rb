76b85a3236bb4bbdd36204ce2ba745410d3ddd49
makevoid <makevoid@gmail.com>
2011-09-21 07:05:00 +0200

    added compass config for prod

--- app/models/rankey.rb
+++ app/models/rankey.rb
@@ -1,8 +1,9 @@
 module Rankey
   # tresholds
+  unless defined?(POS_OK)
+    POS_OK = 30 # a search key over POS_OK it's considered well positioned
   
-  POS_OK = 30 # a search key over POS_OK it's considered well positioned
-  
-  SCRAPE_PAGES = 10 # scrapers will scrape the first SCRAPE_PAGES 
+    SCRAPE_PAGES = 10 # scrapers will scrape the first SCRAPE_PAGES 
+  end
   
 end
--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,3 +1,8 @@
 Rails.configuration.sass.tap do |config|
   config.load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
 end if defined?(Rails.configuration.sass)
+
+Sass::Engine::DEFAULT_OPTIONS[:load_paths].tap do |load_paths|
+  load_paths << "#{Rails.root}/app/assets/stylesheets"
+  load_paths << "#{Gem.loaded_specs['compass'].full_gem_path}/frameworks/compass/stylesheets"
+end
