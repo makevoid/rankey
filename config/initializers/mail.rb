93a3fe6cc61382e65e66bcb82a7628000376e074
Francesco Canessa <makevoid@gmail.com>
2012-07-27 06:54:30 +0200

    fixed mail config for travis

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,11 @@
 MAIL_DEFAULT_FROM = "m4kevoid@gmail.com"
+pass_file = File.expand_path("~/.password")
+
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   user_name:  MAIL_DEFAULT_FROM,
-  password:   File.read(File.expand_path("~/.password")).strip.gsub(/33/, ''),
+  password:   File.exist?(pass_file) ? File.read(File.expand_path("~/.password")).strip.gsub(/33/, '') : "",
   address:    "smtp.gmail.com",
   enable_starttls_auto: true,
   authentication:       :plain,
